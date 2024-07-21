b0e71532bd9c3e4974d6af2a8c3692d6e181ee71
AsifRiseofPower <aloasif31@gmail.com>
2024-07-21 15:19:22 +0530

    z-index

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
     }
   };
   return (
-    <div className="navbar bg-base-100 fixed">
+    <div className="navbar bg-base-100 fixed z-10">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
